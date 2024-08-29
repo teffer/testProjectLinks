62bf63ee9149ea18040b3006c3bf34a3cf51fa62
лум <@DESKTOP-1RO9SM1>
2024-08-29 12:16:23 +0500

    Добавьте файлы проекта.

---
+++ testProjectLinks/Models/Link.cs
@@ -0,0 +1,11 @@
+﻿namespace testProjectLinks.Models
+{
+    public class Link
+    {
+        public string LinkStr { get; set; }
+        public string NewLinkStr { get; set; }
+        public int LinkVisits { get; set; }
+        public DateTime CreationTime { get; set; }
+
+    }
+}
